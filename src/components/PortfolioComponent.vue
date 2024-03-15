d4e65ed76940a973413e809ec186fd3624291379
mlmh3113 <mlmh3113@hotmail.com>
2024-03-15 04:20:25 -0300

    fix issues

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -10,7 +10,7 @@
                 :class="isDark ? 'dark shadow-blue-900/30' : 'text-black bg-white'"
                 v-for="item in projects.slice(0, cantidad)" :key="item.id">
                 <div class=" h-30">
-                    <img class="h-full w-full object-fill object-center" :src="item.image" :alt=item.title loading="lazy">
+                    <img class="h-full w-full object-cover object-center min-h-80 " :src="item.image" :alt=item.title loading="lazy">
                     <div class="flex flex-col p-6">
                         <div class="space-y-2">
                             <h3 class="text-xl font-semibold text-sky-500">{{ item.title }}</h3>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,15 @@
 export const projects = [
     {
         "id": 1,
+        "title": "Live seguros",
+        "description": "Landing page desarrollada con Vue.js y Tailwind",
+        "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1710486439/portfolio/liveSeguros_hkzkir.png",
+        "github": "https://github.com/mlmh3113/live-seguros.git",
+        "live": "https://live-seguros.vercel.app/",
+        "tecnologies":["Vue.js","Tailwind"]
+    },
+    {
+        "id": 2,
         "title": "Api Blog",
         "description": "Api desarrollada con Python y Django Rest Framework. Se realizo el deploy en PythonAnyWhere.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1705064710/portfolio/blogApi_Python_tcrdnz.png",
@@ -9,16 +18,16 @@ export const projects = [
         "tecnologies":["Python","Django Rest Framework"]
     },
     {
-        "id": 2,
+        "id": 3,
         "title": "Morgan Games Blog",
         "description": "A partir de la API Blog previamente creada, se realiza un CRUD de juegos tomando como base el diseño de PlayStation Blog Latam.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1705064704/portfolio/SQL-shotSO_l5toor.png",
         "github": "https://github.com/mlmh3113/drfpython.git",
         "live": "http://martinh3113.pythonanywhere.com/posts/",
-        "tecnologies":["JavaScript","VUE","Tailwind","SQL","Axios","Pinia","HTML","CSS"] 
+        "tecnologies":["JavaScript","Vue.js","Tailwind","SQL","Axios","Pinia","HTML","CSS"] 
     },
     {
-        "id": 3,
+        "id": 4,
         "title": "Fake Bank SQL",
         "description": "Creación de una base de datos en MySQL Workbench para la gestión de clientes, cuentas, préstamos, seguros y empleados de una entidad bancaria.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1705064704/portfolio/SQL-shotSO_l5toor.png",
