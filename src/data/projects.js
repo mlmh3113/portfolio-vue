925c84f1a1f248d5727191e2a128e834729af701
mlmh3113 <mlmh3113@hotmail.com>
2024-04-11 09:14:38 -0300

    data-update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,15 @@
 export const projects = [
     {
-        "id": 1,
+        "id": 9,
+        "title": "Estudio Casa",
+        "description": "Landing Page desarrollada para un estudio de diseño y construccion creada con Vue.js, Javascript y Tailwind",
+        "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1712836772/portfolio/estudioCasa_dgszsj.png",
+        "github": "https://github.com/mlmh3113/Estudio-Casa.git",
+        "live": "https://estudio-casa.vercel.app/",
+        "tecnologies":["Vue.js","JavaScript","Tailwind"]
+    },
+    {
+        "id": 10,
         "title": "Get Fit",
         "description": "Desarrollo Full-stack orientado a la gestión de una página de ejercicios, permitiendo la administración de los alumnos (CRUD), gestión de usuarios, validación con JWT y confirmación por correo electrónico. Realizado el backend con Node.js y Express.js, y el frontend con Vue.js y Tailwind.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1710496088/portfolio/getFit_atmux0.png",
@@ -9,7 +18,7 @@ export const projects = [
         "tecnologies":["Vue.js","Tailwind","Node.js","Express.js","MongoDB","Axios","Pinia","JavaScript","JWT"]
     },
     {
-        "id": 2,
+        "id": 20,
         "title": "Live seguros",
         "description": "Landing page desarrollada para una aseguradora. Se utilizo el framework Vue.js y Tailwind",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1710486439/portfolio/liveSeguros_hkzkir.png",
@@ -18,7 +27,7 @@ export const projects = [
         "tecnologies":["Vue.js","Tailwind","JavaScript"]
     },
     {
-        "id": 3,
+        "id": 30,
         "title": "Api Blog",
         "description": "Api desarrollada con Python y Django Rest Framework. Se realizo el deploy en PythonAnyWhere.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1705064710/portfolio/blogApi_Python_tcrdnz.png",
@@ -27,7 +36,7 @@ export const projects = [
         "tecnologies":["Python","Django Rest Framework"]
     },
     {
-        "id": 4,
+        "id": 40,
         "title": "Morgan Games Blog",
         "description": "A partir de la API Blog previamente creada, se realiza un CRUD de juegos tomando como base el diseño de PlayStation Blog Latam.",
         "image":"https://res.cloudinary.com/dkoocayxp/image/upload/v1705064704/portfolio/SQL-shotSO_l5toor.png",
